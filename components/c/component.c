1b377beee36576eb25560f8e2a72f5d346e82f3f
Kyle Brown <kylebrw@gmail.com>
2023-09-01 16:21:18 -0400

    Fix C components testing python

--- components/c/component.c
+++ components/c/component.c
@@ -9,11 +9,17 @@ void exports_wasmcon2023_greet_interface_greet(greeter_adapter_string_t *ret) {
     greeter_adapter_string_t a;
     greeter_adapter_string_set(&a, " and C!");
 
-    ret->len = greeting.len + a.len;
-    ret->ptr = malloc(ret->len + 1);
+    // TODO: try without extra intermediate string
+    // char* suffix = " and C!";
+    // size_t suffix_len = strlen(suffix);
 
-    strcpy(ret->ptr, greeting.ptr);
-    strcat(ret->ptr, a.ptr);
+    ret->len = greeting.len + a.len;
+    ret->ptr = malloc(ret->len * 2); // TODO: test with +1 instead of *2
+    memset(ret->ptr, 0, ret->len * 2);
+
+    memcpy(ret->ptr, greeting.ptr, greeting.len);
+    memcpy(ret->ptr + greeting.len, a.ptr, a.len);
+
     greeter_adapter_string_free(&a);
     greeter_adapter_string_free(&greeting);
 }
--- runner/src/main.rs
+++ runner/src/main.rs
@@ -3,7 +3,7 @@ use std::{fs, path::PathBuf};
 use anyhow::Result;
 use clap::Parser;
 use wasmtime::{
-    component::{Component, Linker, bindgen},
+    component::{Component, Linker},
     Config, Engine, Store,
 };
 
@@ -11,27 +11,76 @@ use wasmtime::{
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    input: PathBuf,
-}
+    component: PathBuf,
 
-bindgen!("greeter" in "../wit/greeter.wit");
+    #[arg(short, long)]
+    input: Option<String>
+}
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    let component_bytes = fs::read(args.input)?;
+    let component_bytes = fs::read(args.component)?;
 
-    let mut config = Config::new();
-    config.wasm_component_model(true);
-    let engine = Engine::new(&config)?;
+    match args.input {
+        Some(input) => ga::exec(component_bytes, input),
+        None => g::exec(component_bytes),
+    }
+}
 
-    let component = Component::new(&engine, component_bytes)?;
+mod g {
+    use super::*;
+    use wasmtime::component::bindgen;
 
-    let linker = Linker::new(&engine);
-    let mut store = Store::new(&engine, ());
-    let (greeter, _) = Greeter::instantiate(&mut store, &component, &linker)?;
+    bindgen!("greeter" in "../wit/greeter.wit");
 
-    println!("{}", greeter.interface0.call_greet(&mut store)?);
+    pub fn exec(component_bytes: Vec<u8>) -> Result<()> {
+        let mut config = Config::new();
+        config.wasm_component_model(true);
+        let engine = Engine::new(&config)?;
 
-    Ok(())
+        let component = Component::new(&engine, component_bytes)?;
+
+        let linker = Linker::new(&engine);
+        let mut store = Store::new(&engine, ());
+        let (greeter, _) = Greeter::instantiate(&mut store, &component, &linker)?;
+
+        println!("{}", greeter.interface0.call_greet(&mut store)?);
+        Ok(())
+    }
+}
+
+mod ga {
+    use super::*;
+    use anyhow::Ok;
+    use wasmtime::component::bindgen;
+
+    bindgen!("greeter-adapter" in "../wit/greeter.wit");
+
+    struct InputState {
+        input: String
+    }
+
+    impl wasmcon2023::greet::interface::Host for InputState {
+        fn greet(&mut self,) -> wasmtime::Result<String> {
+            Ok(self.input.clone())
+        }
+    }
+
+    pub fn exec(component_bytes: Vec<u8>, input: String) -> Result<()> {
+        let mut config = Config::new();
+        config.wasm_component_model(true);
+        let engine = Engine::new(&config)?;
+
+        let component = Component::new(&engine, component_bytes)?;
+
+        let mut linker = Linker::new(&engine);
+        GreeterAdapter::add_to_linker(&mut linker, |state: &mut InputState| state)?;
+
+        let mut store = Store::new(&engine, InputState { input });
+        let (greeter, _) = GreeterAdapter::instantiate(&mut store, &component, &linker)?;
+
+        println!("{}", greeter.interface0.call_greet(&mut store)?);
+        Ok(())
+    }
 }
